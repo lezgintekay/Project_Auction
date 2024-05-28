397b47f8c2dd42b4d4cbef264947a8aa7b9c3a90
Lezgin Tekay <tekay.lezgin@yandex.com>
2024-05-28 13:21:49 +0300

    update database

---
+++ Project_Auction_Data_Access/Migrations/20240528102012_changeBidStatus.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_Auction_Data_Access.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeBidStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
