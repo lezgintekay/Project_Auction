b53cf5dcfec7c73dc055fe6aebae1b5ff13777b7
Lezgin Tekay <tekay.lezgin@yandex.com>
2024-05-28 13:17:15 +0300

    created enum for bidstatus

--- Project_Auction_Data_Access/Domain/Bid.cs
+++ Project_Auction_Data_Access/Domain/Bid.cs
@@ -1,4 +1,5 @@
-﻿using Project_Auction_Data_Access.Models;
+﻿using Project_Auction_Data_Access.Enums;
+using Project_Auction_Data_Access.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -14,7 +15,7 @@ namespace Project_Auction_Data_Access.Domain
         public int BidId { get; set; }
         public decimal BidAmount { get; set; }
         public DateTime BidDate { get; set; }
-        public string BidStatus { get; set; }
+        public string BidStatus { get; set; } = Project_Auction_Data_Access.Enums.BidStatus.Pending.ToString();
         public string UserId { get; set; }
         public ApplicationUser User{ get; set; }
         public int VehicleId { get; set; }
