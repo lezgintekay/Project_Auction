c90cdb693512df456543a2ea648921f942917ed4
Lezgin Tekay <tekay.lezgin@yandex.com>
2024-05-28 15:29:59 +0300

    jwt configuration for users

--- Project_Auction_Business/Concrete/UserService.cs
+++ Project_Auction_Business/Concrete/UserService.cs
@@ -1,6 +1,12 @@
-﻿using Project_Auction_Business.Abstraction;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Project_Auction_Business.Abstraction;
 using Project_Auction_Business.Dtos;
 using Project_Auction_Core.Models;
+using Project_Auction_Data_Access.Context;
+using Project_Auction_Data_Access.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +17,23 @@ namespace Project_Auction_Business.Concrete
 {
     public class UserService : IUserService
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper; 
+        private readonly ApiResponse _response;
+        private readonly UserManager<ApplicationUser> _userManager; 
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private string secretKey;
+        public UserService(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, ApiResponse response, IMapper mapper, IConfiguration _configuration, ApplicationDbContext context )
+        {
+            _userManager = userManager;
+            _response = response;
+            _mapper = mapper;
+            _context = context;
+            _roleManager = roleManager;
+            secretKey = _configuration.GetValue<string>("SecretKey : jwtKey");
+            
+        }
+
         public Task<ApiResponse> Login(LoginRequestDTO model)
         {
             throw new NotImplementedException();
